80fd2109e1a6a594ae02b97d9088d4f12153d930
guttih <gudjonholm@gmail.com>
2025-08-19 20:55:55 +0000

    adding email to first user login

--- src/app/api/bootstrap/first-user/route.ts
+++ src/app/api/bootstrap/first-user/route.ts
@@ -33,6 +33,7 @@ export async function POST(req: NextRequest) {
     const newUser = await prisma.user.create({
         data: {
             username,
+            email: email || null,
             theme: validTheme,
             passwordHash,
             profileImage: "",
